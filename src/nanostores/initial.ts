c19086910ca7b39c58ce720dd61b6880bd732953
Abigail Dustin <abigail.dustin.21@gmail.com>
2022-10-02 20:57:38 +0400

    Completed ErrorPopUp component

--- src/nanostores/initial.ts
+++ src/nanostores/initial.ts
@@ -2,3 +2,6 @@ import { atom } from 'nanostores'
 
 export type LoginFormState = boolean
 export const loginFormState = atom<LoginFormState>(false)
+
+export type ErrorPopupState = boolean
+export const errorPopupState = atom<ErrorPopupState>(true)
---
+++ src/preact-components/ErrorPopUp.tsx
@@ -0,0 +1,36 @@
+import cs from "./ErrorPopUp.module.sass";
+import { useStore } from "@nanostores/preact";
+import { errorPopupState } from "../nanostores/initial";
+
+type SignMode = "in" | "up"
+type Props = {}
+
+function LoginForm({}: Props) {
+	const windowIsOpen = useStore(errorPopupState)
+	const closeWindow = (e: any) => {
+		const target = e.target as HTMLElement
+		if (!target) return ;
+		console.log(target.className)
+		if (target.className === cs.container ||
+			target.className === cs.container__button_close){
+				errorPopupState.set(false)
+		}
+	}
+
+	if(!windowIsOpen) return null;
+	return(
+		<div className={cs.container} onClick={closeWindow}>
+		<div className={cs.container__box}>
+			<h2 className={cs.container__title}>
+				Gift voucher purchase is not available at this time.
+				<br/>
+				Please try again later.
+			</h2>
+			<button type="reset"
+			className={cs.container__button_close}>close window</button>
+		</div>
+		</div>
+	)
+}
+
+export default LoginForm
