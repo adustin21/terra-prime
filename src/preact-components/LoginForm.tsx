c35de9ed28d28eb06cbd80fb0bee0bb2c8ab864a
Abigail Dustin <abigail.dustin.21@gmail.com>
2022-10-03 00:13:07 +0400

    Added controlled input for sign in

--- src/preact-components/LoginForm.tsx
+++ src/preact-components/LoginForm.tsx
@@ -4,6 +4,8 @@ import { useStore } from "@nanostores/preact";
 import { loginFormState } from "../nanostores/initial";
 
 type SignMode = "in" | "up"
+type SignInData = {login:string, password:string}
+const inititalSignInData: SignInData = {login:"",password:""}
 type Props = {}
 
 function LoginForm({}: Props) {
@@ -11,6 +13,7 @@ function LoginForm({}: Props) {
 	const [signMode, setSignMode] = useState<SignMode>("in")
 	const [wrongAccount, setWrongAccount] = useState<boolean>(false)
 	const [wrongCountry, setWrongCountry] = useState<boolean>(false)
+	const [signInData, setFormData] = useState<SignInData>(inititalSignInData)
 	const closeWindow = (e: any) => {
 		const target = e.target as HTMLElement
 		if (!target) return ;
@@ -19,7 +22,6 @@ function LoginForm({}: Props) {
 				loginFormState.set(false)
 		}
 	}
-
 	if(!windowIsOpen) return null;
 	switch (signMode) {
 		case "in":
@@ -28,13 +30,19 @@ function LoginForm({}: Props) {
 				<div className={cs.container__box}>
 					<div className={cs.container__toggler}>
 						<h2>sign-in</h2>
-						<button onClick={e=>setSignMode("up")}>sign-up</button>
+						<button onClick={e=>{setSignMode("up")}}>
+							sign-up
+						</button>
 					</div>
 					<form className={cs.container__form}
 					onSubmit={e=>{e.preventDefault();setWrongAccount(true)}}>
 					<span>{wrongAccount?"Sorry, we can’t find your account":""}</span>
-					<input required type="text" placeholder="Login" name="login"/>
-					<input required type="password" placeholder="Password" name="password"/>
+					<input required type="text" placeholder="Login" name="login"
+					value={signInData.login}
+					onChange={(e:any)=>signInData.login=e.target.value}/>
+					<input required type="password" placeholder="Password" name="password"
+					value={signInData.password}
+					onChange={(e:any)=>signInData.password=e.target.value}/>
 					<button type="reset"
 					className={cs.container__button_close}>cancel</button>
 					<button type="submit">submit</button>
