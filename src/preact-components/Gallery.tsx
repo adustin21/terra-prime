bf79e06ae641d1cb827a17e9a5d023f39bf307ce
Abigail Dustin <abigail.dustin.21@gmail.com>
2022-10-08 20:22:44 +0400

    Imporved accessebility in Gallery component

--- src/preact-components/Gallery.tsx
+++ src/preact-components/Gallery.tsx
@@ -13,19 +13,17 @@ function Gallery({movieList}: Props) {
 	return (
 		<div className={cs.gallery}>
 			<button
+			aria-label="Scroll to the left."
 			onClick={e=>moveCards(e, 'start')}
 			id={cs.gallery__button_left}
 			className={cs.gallery__button}/>
 			<div className={cs.gallery__cardsContainer}>
 				{movieList.map(movie=>{
-					return(
-						<img
-						{...movie}
-						className={cs.gallery__card}/>
-					)
+					return <img {...movie} className={cs.gallery__card}/>
 				})}
 			</div>
 			<button
+			aria-label="Scroll to the right."
 			onClick={e=>moveCards(e, 'end')}
 			id={cs.gallery__button_right}
 			className={cs.gallery__button}/>
