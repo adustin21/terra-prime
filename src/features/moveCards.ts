880a4c5a3a3e4fdac5957e8f36025b13702efba6
Abigail Dustin <abigail.dustin.21@gmail.com>
2022-10-08 20:41:27 +0400

    Added JSDoc for moveCards function

--- src/features/moveCards.ts
+++ src/features/moveCards.ts
@@ -1,3 +1,8 @@
+/**
+ * Scrolls cs.gallery__cardsContainer.
+ * @param e The "click" event of the gallery scroll button.
+ * @param payload Scroll mode.
+ */
 export const moveCards = (e: any, payload: "start"|"end") => {
 	const cardsContainerElement =
 		e.target?.parentElement?.childNodes?.[1] as HTMLDivElement
