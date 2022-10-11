6c22e6eb90a51c71a31bff2dea901138a5a0a12f
Abigail Dustin <abigail.dustin.21@gmail.com>
2022-10-11 09:34:40 +0400

    Update QuestionBox c. classNames for code style

--- src/preact-components/QuestionBox.tsx
+++ src/preact-components/QuestionBox.tsx
@@ -13,12 +13,12 @@ function QuestionBox({question}: Props) {
 	const [isOpen, setIsOpen] = useState<boolean>(false)
 	return (
 		<div className={`${cs.container} ${isOpen && cs.container_open}`}>
-		<h3 className={cs.container__title}>{question.question}</h3>
-		<button
-		onClick={e=>setIsOpen(!isOpen)}
-		className={cs.container__crossButton}/>
-		<hr></hr>
-		<p className={cs.container__text}>{question.answer}</p>
+			<h3 className={cs.title}>{question.question}</h3>
+			<button
+			onClick={e=>setIsOpen(!isOpen)}
+			className={cs.crossButton}/>
+			<hr className={cs.space}/>
+			<p className={cs.text}>{question.answer}</p>
 		</div>
 	)
 }
