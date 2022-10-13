9207ac21694131ddb15415940befa77a3cf10e8b
Abigail Dustin <abigail.dustin.21@gmail.com>
2022-10-13 14:51:26 +0400

    Add toggle functionality to title of QuestioBox

--- src/preact-components/QuestionBox.tsx
+++ src/preact-components/QuestionBox.tsx
@@ -13,9 +13,12 @@ function QuestionBox({question}: Props) {
 	const [isOpen, setIsOpen] = useState<boolean>(false)
 	return (
 		<div className={`${cs.container} ${isOpen && cs.container_open}`}>
-			<h3 className={cs.title}>{question.question}</h3>
+			<h3 className={cs.title}
+			onClick={e=>setIsOpen(!isOpen)}>
+				{question.question}
+			</h3>
 			<button
-			aria-label={isOpen?"See answer" : "Hide answer"}
+			aria-label={isOpen ? "See answer" : "Hide answer"}
 			onClick={e=>setIsOpen(!isOpen)}
 			className={cs.crossButton}/>
 			<hr className={cs.space}/>
